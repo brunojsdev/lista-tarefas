02ec519b439d677992cb5eeb9e61e4b3545edd4e
Bruno J. Silveira <brunosilveiradevbs@gmail.com>
2026-03-04 01:09:55 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* --- 1. TASK MANAGEMENT LOGIC --- */
 let taskDatabase = [
-    { id: 1, text: "Tarefa concluida („>_<„)", status: true },
-    { id: 2, text: "Escreva uma nova tarefa („• ֊ •„)", status: false }
+    { id: 1, text: "Tarefa concluida ;) ", status: true },
+    { id: 2, text: "Escreva uma nova tarefa  (ツ) ", status: false }
 ];
 
 const inputField = document.getElementById('new-task-input');
